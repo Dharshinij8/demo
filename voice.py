753f09e3e94d378a48279a04fa22be1e9432924c
Dharshinij8 <dharshudharshu148@gmail.com>
2025-08-08 09:57:13 +0530

    Update voice.py

--- voice.py
+++ voice.py
@@ -144,8 +144,7 @@ with tab3:
 
     **Contact:**  
     - Email: dharshudharshu148@gmail.com, acquireness@gmail.com  
-    - Website: [https://yourwebsite.com](https://yourwebsite.com)
-
+    
     ---
     Thank you for using our app! Feel free to contribute or suggest features.
     """)
@@ -175,3 +174,4 @@ with tab3:
             st.image(fpath, use_column_width=True)
     else:
         st.info("No snapshots uploaded yet.")
+
